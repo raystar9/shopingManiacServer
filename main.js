ed30d5e340fe8ebf8b29c2c018fc2ad4633c4a39
KooMyeongHoe <mhk9990@gmail.com>
2017-07-17 10:00:43 +0900

    .....

--- main.js
+++ main.js
@@ -61,7 +61,7 @@ function executeQuery(reqUrl, queryString) {            //매개변수1 : 요청
                 if (err == null) {
                     resultToSend[reqUrl] = sqlRes[0];
                     res.send(resultToSend);
-                    con.end();
+                    con.release();
                 }
                 else {
                     res.send('err occured!');
